e4cf1ecec47b5729116333acd8901f9d8bce3181
Jason Banks <jason@eras.fyi>
2023-05-18 22:26:57 +0200

    Troubleshoot deployment

--- supabase/functions/_shared/supabase-client.ts
+++ supabase/functions/_shared/supabase-client.ts
@@ -6,10 +6,5 @@ export function createClient(req: Request) {
     Deno.env.get("SUPABASE_URL") ?? "",
     // Supabase API ANON KEY - env var exported by default.
     Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
-    // Create client with Auth context of the user that called the function.
-    // This way your row-level-security (RLS) policies are applied.
-    // {
-      // global: { headers: { Authorization: req.headers.get("Authorization")! } },
-    // }
   );
 }
