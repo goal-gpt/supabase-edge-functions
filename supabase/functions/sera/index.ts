8aa99bc9d80c8f6e9df9b674efa7f367b0ab11d1
Jason Banks <jason@eras.fyi>
2023-05-18 02:44:55 +0200

    Enable planning scenario

--- supabase/functions/sera/index.ts
+++ supabase/functions/sera/index.ts
@@ -1,23 +1,35 @@
 import { serve } from "http/server.ts";
 import { answerQuery } from "./answerQuery.ts";
-import { OpenAI } from "langchain/llms/openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 import { corsHeaders } from "../_shared/cors.ts";
+import { createClient } from "../_shared/supabase-client.ts";
 
 serve(async (request: Request) => {
+  try {
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(request);
+    
   if (request.method === "OPTIONS") {
     console.log("Handling CORS preflight request.");
     return new Response("ok", { headers: corsHeaders });
   }
-
+  
   console.log("Handling request.");
-  const { query } = await request.json();
-
-  const llm = new OpenAI({
+  const { message, chat } = await request.json();
+  const model = new ChatOpenAI({
     openAIApiKey: Deno.env.get("OPENAI_API_KEY"),
     temperature: 0,
     modelName: "gpt-3.5-turbo",
     verbose: true,
   });
+  
+  return await answerQuery(model, message, supabaseClient, chat);
+  } catch (error) {
+    console.error(error)
 
-  return await answerQuery(llm, query);
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    });
+  }
 });
